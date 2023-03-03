0e55e8be97ded70f003a90306cd4de0d9319b8ad
Dylan Mooers <dylmooers@gmail.com>
2023-03-03 10:38:17 -0700

    chore: fix tests

--- test/e2e/app.e2e.test.ts
+++ test/e2e/app.e2e.test.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from '~server/app.module';
+import { AppModule } from '~server/root/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -19,6 +19,6 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Hello, undefined');
   });
 });
